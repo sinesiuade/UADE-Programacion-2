7d65556484e8bbc3a5bfff37e2a88e5c56e530a6
sinesiuade <dsinesi@uade.edu.ar>
2026-03-13 09:26:17 -0300

    Update Clase1.java
    feat: Se pide nombre y se saluda al usuario
    docs: Clase1.java

--- 13-03-2026 Clase 1/Clase1.java
+++ 13-03-2026 Clase 1/Clase1.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Clase1 {
     /*
      * Este es el típico hola mundo que usamos cuando comenzamos un nuevo lenguaje
@@ -5,5 +7,13 @@ public class Clase1 {
     public static void main(String[] args) {
         // Salida por terminal o consola de un texto
         System.out.println("Hello, Word!");
+
+        Scanner entrada = new Scanner(System.in);
+
+        String nombre;
+
+        System.out.println("Ingrese su nombre: ");
+        nombre = entrada.nextLine();
+        System.out.println("Hola " + nombre);
     }
 }
