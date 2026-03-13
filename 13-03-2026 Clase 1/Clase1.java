f762ea5bb14739bd17a87f5f23e3abb4cc7b8707
sinesiuade <dsinesi@uade.edu.ar>
2026-03-13 10:26:02 -0300

    Update Clase1.java
    Se actualiza Clase1.java para utilizar la nueva clase Print.java

--- 13-03-2026 Clase 1/Clase1.java
+++ 13-03-2026 Clase 1/Clase1.java
@@ -4,17 +4,17 @@ public class Clase1 {
      */
     public static void main(String[] args) {
         // Salida por terminal o consola de un texto
-        System.out.println("Hello, Word!");
+        Print.println("Hello, Word!");
 
         String nombre;
 
         nombre = Input.inputStrTexto("Ingrese su nombre: ");
-        System.out.println("Hola " + nombre);
+        Print.println("Hola " + nombre);
 
         int numero;
         do {
             numero = Input.inputIntTexto("Ingrese un numero (0 para salir): ");
-            System.out.println("El numero ingresado es: " + numero);
+            Print.println("El numero ingresado es: " + numero);
         } while (numero != 0);
     }
 }
