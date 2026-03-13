d1eb76f254c2b1b96d4f3a6733e2b50e8b844d97
sinesiuade <dsinesi@uade.edu.ar>
2026-03-13 10:25:26 -0300

    Añade Print.java
    Se añade print.java que permite optimizar la impresión en terminal de texto

---
+++ 13-03-2026 Clase 1/Print.java
@@ -0,0 +1,9 @@
+public class Print {
+    public static void println(String texto) {
+        System.out.println(texto);
+    }
+
+    public static void print(String texto) {
+        System.out.print(texto);
+    }
+}
