7883cc2e125558e57eed19767bc0257a9485e304
sinesiuade <dsinesi@uade.edu.ar>
2026-03-13 10:15:22 -0300

    Añade Input.java
    Se añade Input.java que busca simplificar el proceso de obtención de datos por consola

---
+++ 13-03-2026 Clase 1/Input.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+    }
+
+    public static int inputInt() {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        return input;
+    }
+
+    public static int inputIntTexto(String texto) {
+        System.out.println(texto);
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        return input;
+    }
+
+    public static String inputStrTexto(String texto) {
+        System.out.println(texto);
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        return input;
+    }
+}
